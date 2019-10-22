737d7428303ee30b311f1cf703b647a283a88a75
collinwzw <38867788+collinwzw@users.noreply.github.com>
2019-10-21 20:49:43 -0400

    Create practice.py
    + 3 sum

---
+++ practice.py
@@ -0,0 +1,51 @@
+
+class Solution:
+
+#3Sum
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         hashset = []
+        
+#         nums.sort()
+        
+#         for i in range(0,len(nums)-1):
+#             left = i+1
+#             right = len(nums) -1
+#             while (left !=right):
+#                 if nums[i] + nums[left] + nums[right] == 0:
+#                     if [nums[i],nums[left],nums[right]] not in hashset:
+#                         hashset.append([nums[i],nums[left],nums[right]])
+#                     if left + 1 != right:
+#                         left +=1
+#                         right -=1
+#                     else:
+#                         break
+#                 elif nums[i] + nums[left] + nums[right] > 0:
+#                     right -= 1
+#                 elif nums[i] + nums[left] + nums[right] < 0:
+#                     left +=1
+        
+#         return hashset   
+        
+
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1 
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
+
